c7d0d6e2707f3a6a4037cc444fb112894859f6f3
Michael Legemah <michaellegemah@gmail.com>
2023-09-26 20:29:32 -0400

    added jest and supertest for launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches, addNewLaunch, existsLaunchWithId } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches()); // export data access model
