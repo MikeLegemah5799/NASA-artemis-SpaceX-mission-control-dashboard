574248dda6c1ca0eabe1439df3413b8f4fbfe79a
Michael Legemah <michaellegemah@gmail.com>
2023-10-01 17:37:26 -0400

    added schema

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,7 +8,7 @@ const launch = {
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
     target: 'Kepler-442 b',
-    customer: ['MLEG', 'NASA'],
+    customers: ['MLEG', 'NASA'],
     upcoming: true,
     success: true,
 };
