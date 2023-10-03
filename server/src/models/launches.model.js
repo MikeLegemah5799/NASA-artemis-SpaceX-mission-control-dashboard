ef2f6c61d78a7bde4813c61f6c80d64f7d0314a2
Michael Legemah <michaellegemah@gmail.com>
2023-10-03 11:23:47 -0400

    installed axios

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -18,6 +18,10 @@ const launch = {
 
 saveLaunch(launch);
 
+async function loadLaunchesData() {
+    console.log('Downloading launch data...');
+}
+
 async function existsLaunchWithId(launchId) {
     return await launchesDatabase.findOne({
         flightNumber: launchId,
@@ -84,6 +88,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+    loadLaunchesData,
     existsLaunchWithId,
     getAllLaunches,
     scheduleNewLaunch,
